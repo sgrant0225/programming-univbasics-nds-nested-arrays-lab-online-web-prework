b25581cfb94cf77466b8307f269c1fac3bddbb90
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-08 03:10:35 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,7 +40,8 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
- result = matrix_update(matrix, row, column, new_value)
+ result = matrix_update(matrix)
  matrix_update[0][2] = "Tiny Ewell"
  matrix_update
+ result[0]
 end
